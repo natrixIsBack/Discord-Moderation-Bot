6de6b39088bdda8c420f7a9a791ea718f3174c7b
natrixIsBack <gigibbbbj@gmail.com>
2025-05-09 23:21:45 +0200

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`);
+      return;
+    }
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(`Error executing ${interaction.commandName}`);
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  },
+};
